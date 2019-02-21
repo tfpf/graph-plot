584b2880291436a13832816f36d7faeb7ff2d0f1
Vishal Chandratreya <vpaijc@gmail.com>
2019-02-21 15:57:42 +0530

    Update plot_prototype.py

--- plot_prototype.py
+++ plot_prototype.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import matplotlib.patches as mp
 import matplotlib.pyplot as pp
 import numpy as np
+import sys
 
 ################################################################################
 
@@ -29,13 +31,18 @@ ax = fig.add_subplot(1, 1, 1)
 with open('counter.txt') as count_file:
 	graph_id = int(count_file.readline())
 fig.canvas.set_window_title('graph_%x' % graph_id)
-print('run #{}'.format(graph_id))
 with open('counter.txt', 'w') as count_file:
-	count_file.write('%d' % (graph_id + 1))
+	count_file.write('{}'.format(graph_id + 1))
 
 # choose a plot style
-# print(pp.style.available)
-# pp.style.use('classic')
+try:
+	pp.style.use(sys.argv[1])
+except IndexError:
+	print('Plot style not specified. Using default.')
+except OSError:
+	print('Invalid plot style specified. Using default.')
+print('available styles:')
+print(pp.style.available)
 
 # display a thick line for the coordinate axes
 ax.axhline(linewidth = 1.6, color = 'k')
@@ -45,9 +52,9 @@ ax.axvline(linewidth = 1.6, color = 'k')
 
 # the statements which actually plot the graphs
 # t = np.linspace(0, 10 * np.pi, 98257)
-x1 = np.linspace(4, 100, 1e5)
-y1 = (np.sin(x1 ** 2) - x1) / (np.cos(x1) ** 2 - x1)
-ax.plot(x1, y1, 'r-', label = r'$y=\dfrac{\sin\,x^2-x}{\cos^2x-x}$', linewidth = 0.8)
+x1 = np.linspace(-10, 10, 1e5)
+y1 = 1 / x1 - 1 / (np.exp(x1) - 1)
+ax.plot(x1, y1, 'r-', label = r'$y=\dfrac{1}{x}-\dfrac{1}{e^x-1}$', linewidth = 0.8)
 # y2 = np.linspace(-4, 4, 38601)
 # x2 = np.zeros(len(y2))
 # ax.plot(x2, y2, 'r-', linewidth = 0.8)
@@ -57,9 +64,11 @@ ax.plot(x1, y1, 'r-', label = r'$y=\dfrac{\sin\,x^2-x}{\cos^2x-x}$', linewidth =
 # annotations: annotate points in the graph plot
 # argument 1: annotation text
 # argument 2: annotation location
-# argument 3: annotation text locationfrequency of miracles
-# ax.annotate(r'$(0, 0)$', xy = (0, 0), xytext = (0, 0))
-# ax.annotate(r'$(8.5, 11.5)$', xy = (8.5, 11.5), xytext = (7.3, 13.5))
+# argument 3: annotation text location
+# ax.annotate(r'$i^3$', xy = (0, -1), xytext = (0.05, -1.05))
+# ax.add_patch(mp.Circle((0, -1), 0.03))
+# ax.annotate(r'$i$', xy = (0, 1), xytext = (0.05, 0.95))
+# ax.add_patch(mp.Circle((0, 1), 0.03))
 # ax.annotate(r'$(10, 2)$', xy = (10, 2), xytext = (10.1, 2.3))
 # ax.annotate(r'$(10, 10)$', xy = (10, 10), xytext = (10.1, 10.3))
 
@@ -70,12 +79,16 @@ ax.grid(True, linewidth = 0.4)
 ax.legend(loc = 'upper right')
 # ax.set_title('example')
 ax.set_xlabel(r'$x$')
-# ax.set_xlim(-4, 4)
+# ax.set_xlim(-2, 2)
 # ax.set_xticklabels([r'$\mu-4\sigma$', r'$\mu-3\sigma$', r'$\mu-2\sigma$', r'$\mu-\sigma$', r'$\mu$', r'$\mu+\sigma$', r'$\mu+2\sigma$', r'$\mu+3\sigma$', r'$\mu+4\sigma$'])
+# ax.set_xticklabels([r'$-4\pi$', r'$-3\pi$', r'$-2\pi$', r'$-\pi$', r'$0$', r'$\pi$', r'$2\pi$', r'$3\pi$', r'$4\pi$'])
 # ax.set_xticks([i * np.pi for i in range(-4, 5)])
+# ax.set_xticks([4, 6])
+# ax.set_xticklabels(['invention of\ncamera', 'invention of\nphoto editing'])
 ax.set_ylabel(r'$y$')
 # ax.set_ylim(-2, 2)
 # ax.set_yticklabels([r'$\dfrac{0.3989}{\sigma}$', r'$\dfrac{0.2420}{\sigma}$', r'$\dfrac{0.0540}{\sigma}$', r'$\dfrac{0.0044}{\sigma}$'])
 # ax.set_yticks(np.array([0.3989, 0.2420, 0.0540, 0.0044]) / t)
+# ax.set_yticks([0])
 # fig.gca().set_aspect('equal', adjustable = 'box')
 pp.show()
