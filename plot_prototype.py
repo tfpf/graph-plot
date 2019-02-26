249a897e6f5e3ca6468846715c6015e7f0884d0b
Vishal Chandratreya <vpaijc@gmail.com>
2019-02-26 16:22:29 +0530

    Update plot_prototype.py

--- plot_prototype.py
+++ plot_prototype.py
@@ -22,6 +22,32 @@ def tetration(x, n):
 
 ################################################################################
 
+# print a list neatly
+def show_nice_list(items, columns = 2):
+
+	# pad the list
+	while len(items) % columns:
+		items.append(' ')
+	rows = int(len(items) / columns)
+
+	# split the list
+	subitems = []
+	for i in range(rows):
+		subitems.append(items[i :: rows])
+
+	print([len(row[i]) for i in range(columns) for row in subitems])
+	# widths = [max(len(subitems[:][i])) for i in range(columns)]
+	# print(widths)
+
+	for item in subitems:
+		for i in item:
+			sys.stdout.write(i.ljust(25))
+			sys.stdout.flush()
+		print()
+
+
+################################################################################
+
 # open a window to plot a graph
 # a subplot is created so that it can be easily modified to plot multiple
 # set the name of the plot window from the number in 'counter.txt'
@@ -38,11 +64,26 @@ with open('counter.txt', 'w') as count_file:
 try:
 	pp.style.use(sys.argv[1])
 except IndexError:
-	print('Plot style not specified. Using default.')
+	print('Plot style not specified. Using \'classic\'.')
+	pp.style.use('classic')
+	pp.style.use('bmh')
 except OSError:
-	print('Invalid plot style specified. Using default.')
-print('available styles:')
-print(pp.style.available)
+	print('Invalid plot style specified. Using \'classic\'.')
+	pp.style.use('classic')
+	pp.style.use('bmh')
+
+# display available styles
+show_nice_list(pp.style.available, 4)
+# styles = pp.style.available
+# while len(styles) % 3:
+# 	styles.append(' ')
+# left = int(len(styles) / 3)
+# right = 2 * left
+# for k in zip(styles[: left], styles[left : right], styles[right :]):
+# 	for i in k:
+# 		sys.stdout.write(i.ljust(25))
+# 		sys.stdout.flush()
+# 	print()
 
 # display a thick line for the coordinate axes
 ax.axhline(linewidth = 1.6, color = 'k')
@@ -52,12 +93,15 @@ ax.axvline(linewidth = 1.6, color = 'k')
 
 # the statements which actually plot the graphs
 # t = np.linspace(0, 10 * np.pi, 98257)
-x1 = np.linspace(-10, 10, 1e5)
-y1 = 1 / x1 - 1 / (np.exp(x1) - 1)
-ax.plot(x1, y1, 'r-', label = r'$y=\dfrac{1}{x}-\dfrac{1}{e^x-1}$', linewidth = 0.8)
-# y2 = np.linspace(-4, 4, 38601)
-# x2 = np.zeros(len(y2))
-# ax.plot(x2, y2, 'r-', linewidth = 0.8)
+x1 = np.linspace(1, 150, 1e5)
+y1 = (np.sin(x1 ** 2) - x1) / (np.cos(x1) ** 2 - x1)
+ax.plot(x1, y1, 'r-', label = r'$y=\dfrac{\sin\,x^2-x}{\cos^2x-x}$', linewidth = 0.8)
+# x2 = np.linspace(2, 10, 1e5)
+# y2 = x2 - 4
+# ax.plot(x2, y2, 'b-', label = r'$y=x-4$', linewidth = 0.8)
+# x3 = np.linspace(0.2, 10, 1e5)
+# y3 = 1 / x3
+# ax.plot(x3, y3, 'g-', label = r'$y=\dfrac{1}{x}$', linewidth = 0.8)
 
 ################################################################################
 
@@ -76,19 +120,22 @@ ax.plot(x1, y1, 'r-', label = r'$y=\dfrac{1}{x}-\dfrac{1}{e^x-1}$', linewidth = 
 
 # miscellaneous settings
 ax.grid(True, linewidth = 0.4)
-ax.legend(loc = 'upper right')
+ax.legend(loc = 'best')
 # ax.set_title('example')
 ax.set_xlabel(r'$x$')
 # ax.set_xlim(-2, 2)
 # ax.set_xticklabels([r'$\mu-4\sigma$', r'$\mu-3\sigma$', r'$\mu-2\sigma$', r'$\mu-\sigma$', r'$\mu$', r'$\mu+\sigma$', r'$\mu+2\sigma$', r'$\mu+3\sigma$', r'$\mu+4\sigma$'])
 # ax.set_xticklabels([r'$-4\pi$', r'$-3\pi$', r'$-2\pi$', r'$-\pi$', r'$0$', r'$\pi$', r'$2\pi$', r'$3\pi$', r'$4\pi$'])
 # ax.set_xticks([i * np.pi for i in range(-4, 5)])
+ax.set_xticklabels([r'${}\pi$'.format(i) for i in range(5, 51, 5)])
+ax.set_xticks([i * np.pi for i in range(5, 51, 5)])
 # ax.set_xticks([4, 6])
 # ax.set_xticklabels(['invention of\ncamera', 'invention of\nphoto editing'])
 ax.set_ylabel(r'$y$')
-# ax.set_ylim(-2, 2)
+# ax.set_ylim(-4, 4)
 # ax.set_yticklabels([r'$\dfrac{0.3989}{\sigma}$', r'$\dfrac{0.2420}{\sigma}$', r'$\dfrac{0.0540}{\sigma}$', r'$\dfrac{0.0044}{\sigma}$'])
 # ax.set_yticks(np.array([0.3989, 0.2420, 0.0540, 0.0044]) / t)
-# ax.set_yticks([0])
+# ax.set_yticklabels([r'${}$'.format(i) for i in ax.get_yticklabels()])
 # fig.gca().set_aspect('equal', adjustable = 'box')
 pp.show()
+# ax.set_yticks([r'${}$'.format(i) for i in ax.get_yticks()])
